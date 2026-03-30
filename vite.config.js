6115f043066f998ee1f6fca2fdd8917bc0aeac31
LupinC <cy2956743457@gmail.com>
2026-03-30 17:00:20 -0500

    feature: moderninzing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const repository = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+const isUserOrOrgPagesRepo = repository.endsWith(".github.io");
+const base = process.env.GITHUB_ACTIONS
+  ? isUserOrOrgPagesRepo
+    ? "/"
+    : `/${repository}/`
+  : "/";
+
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+  base
 });
